1dddb479b8cc41952d9a1302da74a547483cbd6a
gauravmandal1 <64638825+gauravmandal1@users.noreply.github.com>
2022-02-02 20:18:42 +0530

    completed part 4

--- routes/authors.js
+++ routes/authors.js
@@ -1,48 +1,44 @@
-const express= require('express')
-const router= express.Router()
-const Author = require('../models/authors')
+const express = require('express')
+const router = express.Router()
+const Author = require('../models/author')
 
-//all author routes
+// All Authors Route
 router.get('/', async (req, res) => {
-    let searchOptions = {}
-    if (req.query.name != null && req.query.name !== '') {
-      searchOptions.name = new RegExp(req.query.name, 'i')
-    }
-    try {
-      const authors = await Author.find(searchOptions)
-      res.render('authors/index', {
-        authors: authors,
-        searchOptions: req.query
-      })
-    } catch {
-      res.redirect('/')
-    }
-  })
-
-//new author route  
-
-router.get('/new',(req,res)=>{
-    res.render('authors/new',{author:new Author()})
+  let searchOptions = {}
+  if (req.query.name != null && req.query.name !== '') {
+    searchOptions.name = new RegExp(req.query.name, 'i')
+  }
+  try {
+    const authors = await Author.find(searchOptions)
+    res.render('authors/index', {
+      authors: authors,
+      searchOptions: req.query
+    })
+  } catch {
+    res.redirect('/')
+  }
 })
 
-//createing author route
-router.post('/',async(req,res)=>{
-    const author =new Author({
-        name: req.body.name,
-    })
-    try{
-        const newAuthor= await author.save()
-        //res.redirect(`authors/${newAuthor.id`})
-        res.redirect(`authors`)
-    }
-    catch{
-            let locals ={errorMessage:"Error creating Author"}
-            res.render('authors/new',
-            {
-                author:author,
-                locals:locals
-            })
-        }
-    })
+// New Author Route
+router.get('/new', (req, res) => {
+  res.render('authors/new', { author: new Author() })
+})
 
-module.exports= router
+// Create Author Route
+router.post('/', async (req, res) => {
+  const author = new Author({
+    name: req.body.name
+  })
+  try {
+    const newAuthor = await author.save()
+    // res.redirect(`authors/${newAuthor.id}`)
+    res.redirect(`authors`)
+  } catch {
+    res.render('authors/new', {
+      author: author,
+      errorMessage: 'Error creating Author'
+    })
+  }
+})
+
+module.exports = router
